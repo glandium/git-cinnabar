f2597842bf764715d2f93becd7e4fac1f3473720
Mike Hommey <mh@glandium.org>
2016-01-12 13:20:22 +0900

    Add a VersionedDict class to track changes to a dict

--- cinnabar/util.py
+++ cinnabar/util.py
@@ -165,6 +165,316 @@ class OrderedDefaultDict(OrderedDict):
         return value
 
 
+class VersionedDict(object):
+    def __init__(self, content=None, **kwargs):
+        if content:
+            if kwargs:
+                self._previous = VersionedDict(content)
+                for k, v in kwargs.iteritems():
+                    self._previous[k] = v
+            elif isinstance(content, (VersionedDict, dict)):
+                self._previous = content
+            else:
+                self._previous = dict(content)
+        else:
+            self._previous = dict(**kwargs)
+        self._dict = {}
+        self._deleted = set()
+
+    def update(self, content=None, **kwargs):
+        if content:
+            if isinstance(content, (VersionedDict, dict)):
+                content = content.iteritems()
+            for k, v in content:
+                self[k] = v
+        for k, v in kwargs.iteritems():
+            self[k] = v
+
+    def __getitem__(self, key):
+        if key in self._dict:
+            return self._dict[key]
+        return self._previous[key]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __contains__(self, key):
+        if key in self._deleted:
+            return False
+        if key in self._dict:
+            return True
+        return key in self._previous
+
+    def __delitem__(self, key):
+        self._deleted.add(key)
+        if key in self._dict:
+            del self._dict[key]
+        elif key not in self._previous:
+            raise KeyError(key)
+
+    def __setitem__(self, key, value):
+        if key in self._deleted:
+            self._deleted.remove(key)
+        self._dict[key] = value
+
+    def __len__(self):
+        return len(self._dict) + sum(1 for k in self._previous
+                                     if k not in self._deleted and
+                                     k not in self._dict)
+
+    def keys(self):
+        if self._previous:
+            return list(self)
+        return self._dict.keys()
+
+    def values(self):
+        if self._previous:
+            return list(chain(
+                self._dict.values(),
+                (v for k, v in self._previous.iteritems()
+                 if k not in self._deleted and k not in self._dict)))
+        return self._dict.values()
+
+    def __iter__(self):
+        if self._previous:
+            return chain(self._dict,
+                         (k for k in self._previous
+                          if k not in self._deleted and k not in self._dict))
+        return iter(self._dict)
+
+    def iteritems(self):
+        if self._previous:
+            return chain(
+                self._dict.iteritems(),
+                ((k, v) for k, v in self._previous.iteritems()
+                 if k not in self._deleted and k not in self._dict))
+        return self._dict.iteritems()
+
+    CREATED = 1
+    MODIFIED = 2
+    REMOVED = 3
+
+    def iterchanges(self):
+        for k, v in self._dict.iteritems():
+            if k in self._previous:
+                if self._previous[k] == v:
+                    continue
+                status = self.MODIFIED
+            else:
+                status = self.CREATED
+            yield status, k, v
+        for k in self._deleted:
+            if k in self._previous:
+                yield self.REMOVED, k, self._previous[k]
+
+
+class TestVersionedDict(unittest.TestCase):
+    def check_state(self, v, d):
+        self.assertEquals(sorted(v.keys()), sorted(d.keys()))
+        self.assertEquals(sorted(k for k in v), sorted(k for k in d))
+        self.assertEquals(sorted(i for i in v.iteritems()),
+                          sorted(i for i in d.iteritems()))
+        self.assertEquals(sorted(v.values()), sorted(d.values()))
+        self.assertEquals(bool(d), bool(v))
+        self.assertEquals(len(d), len(v))
+        for k, val in d.iteritems():
+            self.assertTrue(k in v)
+            self.assertEquals(v[k], val)
+
+    def test_versioned_dict(self):
+        for typ in (dict, VersionedDict):
+            for init in ({}, {'hoge': 'hoge'},
+                         {'hoge': 'hoge', 'fuga': 'fuga'}):
+                d = {}
+                d.update(init)
+                v = typ(**init)
+                self.check_state(v, d)
+                v.update(foo='foo', bar='qux')
+                d.update(foo='foo', bar='qux')
+                self.check_state(v, d)
+                self.assertEquals(v['foo'], 'foo')
+                self.assertEquals(v['bar'], 'qux')
+                with self.assertRaises(KeyError):
+                    v['qux']
+                with self.assertRaises(KeyError):
+                    del v['qux']
+                self.assertTrue('foo' in v)
+                self.assertTrue('bar' in v)
+                self.assertFalse('qux' in v)
+
+                if 'fuga' in init:
+                    self.check_state(v, d)
+                    self.assertTrue('fuga' in v)
+                    self.assertEquals(v['fuga'], 'fuga')
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                        ])
+
+                    v['fuga'] = 'toto'
+                    d['fuga'] = 'toto'
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                            (v.MODIFIED, 'fuga', 'toto'),
+                        ])
+
+                    del v['fuga']
+                    del d['fuga']
+                    self.assertFalse('fuga' in v)
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                            (v.REMOVED, 'fuga', 'fuga'),
+                        ])
+                elif 'hoge' in init:
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                        ])
+
+                    del v['hoge']
+                    del d['hoge']
+                    self.assertFalse('hoge' in v)
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                            (v.REMOVED, 'hoge', 'hoge'),
+                        ])
+
+                    v['hoge'] = 'fuga'
+                    d['hoge'] = 'fuga'
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                            (v.MODIFIED, 'hoge', 'fuga'),
+                        ])
+
+                    del v['hoge']
+                    del d['hoge']
+                    self.assertFalse('hoge' in v)
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                            (v.REMOVED, 'hoge', 'hoge'),
+                        ])
+
+                    v['hoge'] = 'hoge'
+                    d['hoge'] = 'hoge'
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                            (v.CREATED, 'foo', 'foo'),
+                        ])
+
+                else:
+                    self.check_state(v, d)
+
+                    del v['foo']
+                    del d['foo']
+                    self.assertFalse('foo' in v)
+                    self.check_state(v, d)
+                    if typ == VersionedDict:
+                        self.assertEquals(sorted(v.iterchanges()), [
+                            (v.CREATED, 'bar', 'qux'),
+                        ])
+
+                for k in v.keys():
+                    del v[k]
+                    del d[k]
+                    self.check_state(v, d)
+
+    def test_init(self):
+        d = {'foo': 'foo', 'bar': 'qux'}
+        d1 = VersionedDict(d)
+        self.assertIs(d1._previous, d)
+
+        d1 = VersionedDict(**d)
+        self.assertIsInstance(d1._previous, dict)
+        self.assertIsNot(d1._previous, d)
+        self.assertEquals(d1._previous, d)
+
+        d1 = VersionedDict(d.iteritems())
+        self.assertIsInstance(d1._previous, dict)
+        self.assertIsNot(d1._previous, d)
+        self.assertEquals(d1._previous, d)
+
+        d1 = VersionedDict(d, bar='bar', hoge='hoge', fuga='toto')
+        self.assertIsInstance(d1._previous, VersionedDict)
+        self.assertEquals(sorted(d1._previous.iterchanges()), [
+            (d1.CREATED, 'fuga', 'toto'),
+            (d1.CREATED, 'hoge', 'hoge'),
+            (d1.MODIFIED, 'bar', 'bar'),
+        ])
+
+        d2 = VersionedDict(d1)
+        self.assertIs(d2._previous, d1)
+
+        d2 = VersionedDict(d1, hoge=42, toto='titi')
+        self.assertIsInstance(d2._previous, VersionedDict)
+        self.assertIsNot(d2._previous, d1)
+        self.assertEquals(sorted(d2._previous.iterchanges()), [
+            (d1.CREATED, 'toto', 'titi'),
+            (d1.MODIFIED, 'hoge', 42),
+        ])
+
+    def test_update(self):
+        d1 = {'a': 1, 'b': 2}
+        d2 = {'c': 3, 'd': 4}
+
+        for content, kwargs in ((d1, {}), ((), d2), (d1, d2)):
+            d = {}
+            v = VersionedDict()
+            args = content and [content]
+            d.update(*args, **kwargs)
+            v.update(*args, **kwargs)
+            self.check_state(v, d)
+
+    def test_change_previous(self):
+        d = {'foo': 'foo', 'bar': 'qux'}
+        d1 = VersionedDict(d)
+
+        d1['hoge'] = 'hoge'
+        del d1['hoge']
+        self.assertTrue('hoge' not in d1)
+        self.assertEquals(sorted(d1.iterchanges()), [])
+
+        d['hoge'] = 'fuga'
+        self.assertTrue('hoge' not in d1)
+        self.assertEquals(sorted(d1.iterchanges()), [
+            (d1.REMOVED, 'hoge', 'fuga'),
+        ])
+
+        d1['foo'] = 'foo'
+        self.assertEquals(sorted(d1.iterchanges()), [
+            (d1.REMOVED, 'hoge', 'fuga'),
+        ])
+
+        d['foo'] = 'bar'
+        self.assertEqual(d1['foo'], 'foo')
+        self.assertEquals(sorted(d1.iterchanges()), [
+            (d1.MODIFIED, 'foo', 'foo'),
+            (d1.REMOVED, 'hoge', 'fuga'),
+        ])
+
+
 def _iter_diff_blocks(a, b):
     m = SequenceMatcher(a=a, b=b, autojunk=False).get_matching_blocks()
     for start, end in izip(chain((Match(0, 0, 0),), m), m):
