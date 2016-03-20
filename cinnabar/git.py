2a03a13af80835eac82cfdb27abc263a1e3292aa
Mike Hommey <mh@glandium.org>
2016-03-20 11:49:19 +0900

    Fix upgrading graft replace ref
    d483c644 made fsck upgrade to the new graft scheme, but in some cases,
    it was removing the replace refs, breaking mercurial metadata for early
    changesets.
    
    This was only a problem when upgrading from the initial graft support
    before the release of 0.3.0 (that wasn't part of any release) to the
    graft support from 0.3.0.

--- cinnabar/git.py
+++ cinnabar/git.py
@@ -121,12 +121,12 @@ class GitProcess(object):
         if env:
             full_env.update(env)
 
-        if args[0] == 'config':
+        if args[0] == 'config' or not Git._replace:
             # We don't need the replace ref setup for config.
             pass
         elif not check_enabled('replace') and HAS_REPLACE_REF_BASE:
             full_env['GIT_REPLACE_REF_BASE'] = 'refs/cinnabar/replace/'
-        elif Git._replace:
+        else:
             if not GitProcess._git_replace_path:
                 from tempfile import mkdtemp
                 # There are commands run before Git._replace is filled, but we
