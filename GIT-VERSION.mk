glandium/git-cinnabar

commit 46191f5ad62881df8f764720a54744ef4ef11d99

[CI] Use a separate file containing the git version to determine the helper hash

GIT-VERSION.mk
@@ -1,0 +1,2 @@
+GIT_VERSION = v2.10.2
+WINDOWS_GIT_VERSION = $(GIT_VERSION).windows.1

.travis.yml
@@ -56,12 +56,12 @@ matrix:
     - env: UPGRADE_FROM=0.2.0
     - env: UPGRADE_FROM=0.1.0
     - env:
-      - GIT_VERSION=2.7.4
+      - GIT_VERSION=v2.7.4
       - UPGRADE_FROM=0.2.2
-    - env: GIT_VERSION=2.5.0
-    - env: GIT_VERSION=2.2.0
-    - env: GIT_VERSION=2.0.0
-    - env: GIT_VERSION=1.8.5
+    - env: GIT_VERSION=v2.5.0
+    - env: GIT_VERSION=v2.2.0
+    - env: GIT_VERSION=v2.0.0
+    - env: GIT_VERSION=v1.8.5
     - env: MERCURIAL_VERSION=installed
       addons:
         apt:

CI.mk
@@ -1,5 +1,5 @@
 OS_NAME = $(TRAVIS_OS_NAME)$(MSYSTEM)
-WINDOWS_GIT_VERSION = v2.10.2.windows.1
+include $(addsuffix /,$(dir $(firstword $(MAKEFILE_LIST))))GIT-VERSION.mk
 
 ifeq (a,$(firstword a$(subst /, ,$(abspath .))))
 PATHSEP = :
@@ -20,7 +20,7 @@ REPO ?= https://bitbucket.org/cleonello/jqplot
 
 -include CI-data.mk
 
-HELPER_HASH := $(shell git ls-tree -r HEAD | grep '\(\.[ch]\|\sgit-core\)$$' | awk '{print $$3}' | shasum | awk '{print $$1}')
+HELPER_HASH := $(shell git ls-tree -r HEAD | grep '\(\.[ch]\|\sGIT-VERSION.mk\)$$' | awk '{print $$3}' | shasum | awk '{print $$1}')
 HELPER_PATH := artifacts/$(HELPER_HASH)/$(OS_NAME)$(addprefix -,$(VARIANT))
 
 helper_hash:
@@ -64,7 +64,7 @@ endif
 
 else
 
-ifdef GIT_VERSION
+ifneq (file,$(origin GIT_VERSION))
 # When building and using a separate git version, disable the helper. It's
 # being tested in many different setups, and hides the interactions with git.
 GIT_CINNABAR_HELPER=
@@ -75,7 +75,7 @@ GIT=$(CURDIR)/git.git/bin-wrappers/git
 before_script::
 	git submodule update --init
 	git clone -n git-core git.git
-	git -C git.git checkout v$(GIT_VERSION)
+	git -C git.git checkout $(GIT_VERSION)
 	$(MAKE) -C git.git --jobs=2 NO_GETTEXT=1 NO_CURL=1 NO_OPENSSL=1
 
 else
@@ -115,7 +115,9 @@ endif
 
 $(GIT_CINNABAR_HELPER):
 	git submodule update --init
-ifneq (,$(filter MINGW%,$(OS_NAME)))
+ifeq (,$(filter MINGW%,$(OS_NAME)))
+	test $$(git -C git-core rev-parse HEAD) = $$(git -C git-core rev-parse $(GIT_VERSION)^{commit})
+else
 	git -C git-core remote add git4win https://github.com/git-for-windows/git
 	git -C git-core remote update git4win
 	git -C git-core merge-base --is-ancestor HEAD $(WINDOWS_GIT_VERSION)

commit 2ce3afece1a6b41733710b82bee2681ed7727323

[CI] Actually use overridden git version
This broke in 46191f5.

GIT-VERSION.mk
@@ -1,2 +1,2 @@
-GIT_VERSION = v2.11.0
-WINDOWS_GIT_VERSION = $(GIT_VERSION).windows.1
+GIT_VERSION ?= v2.11.0
+WINDOWS_GIT_VERSION ?= $(GIT_VERSION).windows.1