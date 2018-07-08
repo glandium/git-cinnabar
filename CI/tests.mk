a055edbe36c3034e814a66e3896b144df4a24fef
Mike Hommey <mh@glandium.org>
2018-07-08 14:25:43 +0900

    [CI] Run full fsck during tests

--- CI/tests.mk
+++ CI/tests.mk
@@ -50,7 +50,7 @@ endif
 hg.upgraded.git: hg.old.git
 	cp -r $< $@
 	$(GIT) -C $@ cinnabar upgrade || [ "$$?" = 2 ]
-	$(GIT) -C $@ cinnabar fsck || [ "$$?" = 2 ]
+	$(GIT) -C $@ cinnabar fsck --manifest --files || [ "$$?" = 2 ]
 
 PATH_URL = file://$(if $(filter /%,$(CURDIR)),,/)$(CURDIR)
 
@@ -106,7 +106,7 @@ hg.unbundle.git: hg.bundle hg.git
 hg.unbundle.git hg.git hg.git.nobundle2:
 	$(GIT) -c fetch.prune=true clone -n hg::$(CURDIR)/$< $@
 	$(call COMPARE_REFS, $(word 2,$^), $@)
-	$(GIT) -C $@ cinnabar fsck
+	$(GIT) -C $@ cinnabar fsck --manifest --files
 	$(GIT) -C $@ remote add hg-http hg::http://localhost:8000/
 
 hg.incr.hg hg.incr.hg.nobundle2: hg.incr.hg%: hg.hg%
@@ -120,7 +120,7 @@ hg.incr.git hg.incr.git.nobundle2: hg.incr.git%: hg.incr.hg% hg.hg% hg.git%
 	$(HG) -R $@.hg pull $(CURDIR)/$(word 2,$^)
 	$(GIT) -C $@ remote update
 	$(call COMPARE_REFS, $(word 3,$^), $@)
-	$(GIT) -C $@ cinnabar fsck
+	$(GIT) -C $@ cinnabar fsck --manifest --files
 
 hg.incr.bundle: hg.incr.hg
 hg.full.bundle: hg.hg
@@ -169,7 +169,7 @@ hg.cinnabarclone.git hg.cinnabarclone-full.git hg.cinnabarclone-graft.git hg.cin
 	echo $(PATH_URL)/$(word 2,$^) > $@.hg/.hg/cinnabar.manifest
 	$(HG) -R $@.hg --config extensions.x=$(TOPDIR)/CI/hg-serve-exec.py --config extensions.cinnabarclone=$(TOPDIR)/hg/cinnabarclone.py serve-and-exec -- $(GIT) -c cinnabar.experiments=git-clone clone hg://localhost:8000.http/ $@
 	$(call COMPARE_REFS, $(or $(word 3,$^),$(word 2,$^)), $@)
-	$(GIT) -C $@ cinnabar fsck
+	$(GIT) -C $@ cinnabar fsck --manifest --files
 
 GET_ROOTS = $(GIT) -C $1 rev-list $2 --max-parents=0
 XARGS_GIT2HG = xargs $(GIT) -C $1 cinnabar git2hg
@@ -186,7 +186,7 @@ hg.graft.git: hg.graft.base.git hg.upgraded.git
 	$(GIT) -C $@ filter-branch --msg-filter 'cat ; echo' --original original -- --all
 	$(GIT) -C $@ -c cinnabar.graft=true remote update
 	$(call COMPARE_REFS, $(word 2,$^), $@, XARGS_GIT2HG)
-	$(GIT) -C $@ cinnabar fsck
+	$(GIT) -C $@ cinnabar fsck --manifest --files
 
 hg.graft2.git: hg.graft.git hg.pure.hg hg.graft2.base.git
 	cp -r $(word 3,$^) $@
@@ -194,7 +194,7 @@ hg.graft2.git: hg.graft.git hg.pure.hg hg.graft2.base.git
 	$(GIT) -C $@ remote set-url origin hg::$(PATH_URL)/$(word 2,$^)
 	$(GIT) -C $@ -c cinnabar.graft=true cinnabar reclone
 	$(call COMPARE_REFS, $<, $@)
-	$(GIT) -C $@ cinnabar fsck
+	$(GIT) -C $@ cinnabar fsck --manifest --files
 
 hg.graft.replace.git: hg.graft.git hg.upgraded.git
 	cp -r $< $@
@@ -203,7 +203,7 @@ hg.graft.replace.git: hg.graft.git hg.upgraded.git
 	$(GIT) -C $@ -c cinnabar.graft=true remote update
 	$(call COMPARE_REFS, $(word 2,$^), $@, XARGS_GIT2HG)
 	$(call COMPARE_COMMANDS,$(call GET_ROOTS,$(word 2,$^),--remotes),$(call GET_ROOTS,$@,--glob=refs/cinnabar/replace))
-	$(GIT) -C $@ cinnabar fsck
+	$(GIT) -C $@ cinnabar fsck --manifest --files
 
 hg.cant.graft.git: hg.graft.replace.git
 	cp -r $< $@
