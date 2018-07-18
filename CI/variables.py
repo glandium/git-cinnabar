3e24d0dba3c5b602e7308fa06ef6dd3e94ba5773
Mike Hommey <mh@glandium.org>
2018-07-18 14:05:57 +0900

    try

--- CI/variables.py
+++ CI/variables.py
@@ -1,21 +1,17 @@
 import os
 
 
-if 'TASKCLUSTER_PROXY' in os.environ:
+if 'TC_PROXY' in os.environ:
     PROXY_INDEX_URL = 'http://taskcluster/index/v1/task/{}'
 else:
     PROXY_INDEX_URL = 'https://index.taskcluster.net/v1/task/{}'
 ARTIFACT_URL = 'https://queue.taskcluster.net/v1/task/{}/artifacts/{}'
 
-GITHUB_HEAD_USER = os.environ.get('GITHUB_HEAD_USER', 'glandium')
-GITHUB_HEAD_USER_EMAIL = os.environ.get('GITHUB_HEAD_USER_EMAIL', 'glandium@')
-GITHUB_HEAD_REPO_NAME = os.environ.get('GITHUB_HEAD_REPO_NAME', 'git-cinnabar')
-GITHUB_HEAD_REPO_URL = os.environ.get(
-    'GITHUB_HEAD_REPO_URL',
-    'https://github.com/{}/{}'.format(GITHUB_HEAD_USER, GITHUB_HEAD_REPO_NAME))
-GITHUB_HEAD_SHA = os.environ.get('GITHUB_HEAD_SHA', 'HEAD')
-GITHUB_HEAD_BRANCH = os.environ.get('GITHUB_HEAD_BRANCH', 'HEAD')
-GITHUB_BASE_USER = os.environ.get('GITHUB_BASE_USER', GITHUB_HEAD_USER)
-GITHUB_BASE_REPO_NAME = os.environ.get('GITHUB_BASE_REPO_NAME',
-                                       GITHUB_HEAD_REPO_NAME)
-GITHUB_EVENT = os.environ.get('GITHUB_EVENT', '')
+TC_LOGIN = os.environ.get('TC_LOGIN', 'glandium')
+TC_REPO_NAME = os.environ.get('TC_REPO_NAME', 'git-cinnabar')
+TC_REPO_URL = os.environ.get(
+    'TC_REPO_URL',
+    'https://github.com/{}/{}'.format(TC_LOGIN, TC_REPO_NAME))
+TC_COMMIT = os.environ.get('TC_COMMIT', 'HEAD')
+TC_BRANCH = os.environ.get('TC_BRANCH')
+TC_IS_PUSH = os.environ.get('TC_IS_PUSH') == '1'
