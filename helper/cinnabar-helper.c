cf440d651b37b39a1712f76ee36541c01337d111
Mike Hommey <mh@glandium.org>
2018-12-11 15:33:51 +0900

    Update git-core to v2.20.0

--- helper/cinnabar-helper.c
+++ helper/cinnabar-helper.c
@@ -1461,7 +1461,7 @@ static void upgrade_files(const struct old_manifest_tree *tree,
 				goto corrupted;
 		}
 		display_progress(track->progress,
-		                 hashmap_get_size(&track->set.map.map));
+		                 kh_size(&track->set.set));
 	}
 
 	free_tree_buffer(state.tree_git);
