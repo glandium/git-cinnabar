7e2b66ac598214391e1d1aceed5801425a5ee9cd
Mike Hommey <mh@glandium.org>
2019-03-17 14:13:21 +0900

    Use split buffers for buffered writed

--- helper/cinnabar-util.c
+++ helper/cinnabar-util.c
@@ -1,4 +1,5 @@
 #include "cache.h"
+#include "thread-utils.h"
 #include "cinnabar-util.h"
 
 #ifdef NO_CURL
@@ -26,33 +27,71 @@ size_t copy_to(FILE *in, size_t len, struct writer *writer)
 	return ret;
 }
 
+#define BUFFER_SIZE (1024 * 1024)
+
 struct buffered_context {
-	struct strbuf buf;
+	char *current_buf;
+	size_t left;
+	char **buffers;
+	size_t nr_buffers;
+	size_t alloc_buffers;
+	size_t buffered;
 	struct writer out;
 };
+
+static void create_buffer(struct buffered_context *context) {
+	ALLOC_GROW(context->buffers, context->nr_buffers + 1, context->alloc_buffers);
+	context->buffers[context->nr_buffers] = xmalloc(BUFFER_SIZE);
+	context->current_buf = context->buffers[context->nr_buffers++];
+	context->left = BUFFER_SIZE;
+}
 
 static size_t buffered_write(char *ptr, size_t eltsize, size_t nmemb, void *context_)
 {
 	struct buffered_context *context = context_;
-	return fwrite_buffer(ptr, eltsize, nmemb, &context->buf);
+	size_t len = eltsize * nmemb;
+	do {
+		size_t fill = len > context->left ? context->left : len;
+		memcpy(context->current_buf, ptr, fill);
+		ptr += fill;
+		len -= fill;
+		if (fill == context->left) {
+			create_buffer(context);
+		} else {
+			context->current_buf += fill;
+			context->left -= fill;
+		}
+		context->buffered += fill;
+	} while (len);
+	return eltsize * nmemb;
 }
 
 static int buffered_close(void *context_)
 {
 	struct buffered_context *context = context_;
+	size_t i;
 	int ret;
-	write_to(context->buf.buf, 1, context->buf.len, &context->out);
+	for (i = 0; context->buffered; i++) {
+		size_t fill = context->buffered > BUFFER_SIZE ?
+			BUFFER_SIZE : context->buffered;
+		write_to(context->buffers[i], 1, fill, &context->out);
+		free(context->buffers[i]);
+		context->buffered -= fill;
+	}
 	ret = writer_close(&context->out);
+	free(context->buffers);
 	free(context);
 	return ret;
 }
 
 void bufferize_writer(struct writer *writer)
 {
-	struct buffered_context *context = xmalloc(sizeof(struct buffered_context));
-	strbuf_init(&context->buf, 1024 * 1024);
-	context->out = *writer;
-	writer->write = buffered_write;
-	writer->close = buffered_close;
-	writer->context = context;
+	if (HAVE_THREADS) {
+		struct buffered_context *context = xcalloc(1, sizeof(struct buffered_context));
+		create_buffer(context);
+		context->out = *writer;
+		writer->write = buffered_write;
+		writer->close = buffered_close;
+		writer->context = context;
+	}
 }
