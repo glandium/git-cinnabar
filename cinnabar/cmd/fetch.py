d2d064fe5fcf4b51cffc78dce75f589eb0e0afd0
Mike Hommey <mh@glandium.org>
2019-04-09 21:36:14 +0900

    Allow to pass multiple revisions to `git cinnabar fetch`

--- cinnabar/cmd/fetch.py
+++ cinnabar/cmd/fetch.py
@@ -14,31 +14,36 @@ from cinnabar.hg.repo import (
 
 @CLI.subcommand
 @CLI.argument('remote', help='mercurial remote name or url')
-@CLI.argument('rev', help='mercurial changeset to fetch')
+@CLI.argument('revs', nargs='+', help='mercurial changeset to fetch')
 def fetch(args):
     '''fetch a changeset from a mercurial remote'''
 
     remote = args.remote
-    rev = args.rev
-    if not re.match('[0-9a-f]{40}$', rev.lower()):
-        if remote.startswith('hg:'):
-            url = remote
-        else:
-            url = Git.config('remote.%s.url' % remote)
-        if not url:
-            print >>sys.stderr, "Unknown remote:", remote
-            return 1
-        if url.startswith('hg::'):
-            url = url[4:]
-        repo = get_repo(Remote(remote, url))
-        if repo.capable('lookup'):
-            rev = hexlify(repo.lookup(rev))
-        else:
-            print >>sys.stderr, (
-                'Remote repository does not support the "lookup" command. '
-                'Please use a non-abbreviated mercurial revision.')
-            return 1
+    revs = args.revs
+    full_revs = []
+    for rev in revs:
+        if not re.match('[0-9a-f]{40}$', rev.lower()):
+            if remote.startswith('hg:'):
+                url = remote
+            else:
+                url = Git.config('remote.%s.url' % remote)
+            if not url:
+                print >>sys.stderr, "Unknown remote:", remote
+                return 1
+            if url.startswith('hg::'):
+                url = url[4:]
+            repo = get_repo(Remote(remote, url))
+            if repo.capable('lookup'):
+                rev = hexlify(repo.lookup(rev))
+            else:
+                print >>sys.stderr, (
+                    'Remote repository does not support the "lookup" command. '
+                    'Please use a non-abbreviated mercurial revision.')
+                return 1
+        full_revs.append(rev)
 
-    proc = GitProcess('fetch', remote, 'hg/revs/%s' % rev, stdout=sys.stdout,
-                      config={'cinnabar.fetch': rev})
+    refs = ['hg/revs/%s' % rev for rev in full_revs]
+
+    proc = GitProcess('fetch', remote, *refs, stdout=sys.stdout,
+                      config={'cinnabar.fetch': ' '.join(full_revs)})
     return proc.wait()
