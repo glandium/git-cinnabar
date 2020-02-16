dbb0f11971c3fd3ed53efa898ed4080c48a11fa3
Mike Hommey <mh@glandium.org>
2020-02-16 15:47:14 +0900

    Remove declaration with no corresponding definition

--- helper/hg-connect-internal.h
+++ helper/hg-connect-internal.h
@@ -8,6 +8,4 @@ void split_capabilities(struct hg_connection *conn, const char *buf);
 struct hg_connection *hg_connect_stdio(const char *url, int flags);
 struct hg_connection *hg_connect_http(const char *url, int flags);
 
-struct hg_connection *hg_connect_bundle(const char *path);
-
 #endif
