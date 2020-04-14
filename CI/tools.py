a7fe63b7cfa02bb9db2f573d3d45cfb7cab70a95
Mike Hommey <mh@glandium.org>
2020-04-15 05:13:59 +0900

    Update git-core to v2.26.1

--- CI/tools.py
+++ CI/tools.py
@@ -12,7 +12,7 @@ import msys
 
 
 MERCURIAL_VERSION = '5.3'
-GIT_VERSION = '2.26.0'
+GIT_VERSION = '2.26.1'
 
 ALL_MERCURIAL_VERSIONS = (
     '1.9.3', '2.0.2', '2.1.2', '2.2.3', '2.3.2', '2.4.2', '2.5.4',
