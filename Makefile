glandium/git-cinnabar

commit 6a7658b7e84736cd5ab71cd1345cf92d45ec435b

Always use the git-for-windows copy of git-core
This simplifies the submodule setup, and is fine for what we use the git
code for.

Makefile
@@ -1,11 +1,4 @@
 SYSTEM = $(shell python2.7 -c 'import platform; print platform.system()')
-include helper/GIT-VERSION.mk
-ifeq ($(SYSTEM),Windows)
-GIT_REPO = https://github.com/git-for-windows/git
-GIT_VERSION := $(WINDOWS_GIT_VERSION)
-else
-GIT_REPO = $(shell sed -n 's/.*url = //p' .gitmodules)
-endif
 SUBMODULE_STATUS := $(shell git submodule status git-core 2> /dev/null || echo no)
 
 define exec
@@ -16,20 +9,12 @@ endif
 endef
 
 ifeq ($(SUBMODULE_STATUS),no)
+GIT_REPO = $(shell sed -n 's/.*url = //p' .gitmodules)
 $(eval $(call exec,git clone -n $(GIT_REPO) git-core))
 $(eval $(call exec,git -C git-core checkout $(GIT_VERSION)))
 else
-ifneq ($(shell git -C git-core rev-parse HEAD),$(shell git -C git-core rev-parse --revs-only $(GIT_VERSION)^{commit}))
+$(eval $(call exec,git submodule sync))
 $(eval $(call exec,git submodule update --init))
-ifeq ($(SYSTEM),Windows)
-$(eval $(call exec,git -C git-core remote add git4win $(GIT_REPO)))
-$(eval $(call exec,git -C git-core fetch git4win --tags))
-$(eval $(call exec,git -C git-core checkout $(GIT_VERSION)))
-endif
-endif
-endif
-ifneq ($(shell git -C git-core rev-parse HEAD),$(shell git -C git-core rev-parse --revs-only $(GIT_VERSION)^{commit}))
-$(error git-core is not checked out at $(GIT_VERSION))
 endif
 
 .PHONY: helper

.gitmodules
@@ -1,3 +1,3 @@
 [submodule "git-core"]
 	path = git-core
-	url = https://github.com/git/git
+	url = https://github.com/git-for-windows/git

CI/package.mk
@@ -1,7 +1,5 @@
 TOPDIR = $(abspath $(or $(dir $(firstword $(MAKEFILE_LIST))),$(CURDIR))/..)
 
-include $(TOPDIR)/helper/GIT-VERSION.mk
-
 ifeq (a,$(firstword a$(subst /, ,$(abspath .))))
 PATHSEP = :
 else

CI/tests.mk
@@ -1,7 +1,5 @@
 TOPDIR = $(abspath $(or $(dir $(firstword $(MAKEFILE_LIST))),$(CURDIR))/..)
 
-include $(TOPDIR)/helper/GIT-VERSION.mk
-
 ifeq (a,$(firstword a$(subst /, ,$(abspath .))))
 PATHSEP = :
 else

helper/GIT-VERSION.mk
@@ -1,2 +1,0 @@
-GIT_VERSION ?= v2.31.1
-WINDOWS_GIT_VERSION ?= $(GIT_VERSION).windows.1

commit c684b03c83d38efa802868eecb876c15b65f0033

Remove code to build out of a source zip
This has actually been broken since 6a7658b.

Makefile
@@ -1,5 +1,3 @@
-SUBMODULE_STATUS := $(shell git submodule status git-core 2> /dev/null || echo no)
-
 define exec
 $$(shell echo $1 >&2)
 ifeq (fail,$$(shell $1 >&2 || echo fail))
@@ -7,14 +5,8 @@ $$(error failed)
 endif
 endef
 
-ifeq ($(SUBMODULE_STATUS),no)
-GIT_REPO = $(shell sed -n 's/.*url = //p' .gitmodules)
-$(eval $(call exec,git clone -n $(GIT_REPO) git-core))
-$(eval $(call exec,git -C git-core checkout $(GIT_VERSION)))
-else
 $(eval $(call exec,git submodule sync))
 $(eval $(call exec,git submodule update --init))
-endif
 
 .PHONY: helper
 helper: