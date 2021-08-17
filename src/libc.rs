9c0f4c896d6225bf460aad00e3ea2638967fef4a
Mike Hommey <mh@glandium.org>
2021-08-17 19:39:17 +0900

    Remove now unused libc::File

--- src/libc.rs
+++ src/libc.rs
@@ -2,65 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use std::convert::TryInto;
 use std::ffi::c_void;
-use std::io::{self, Read, Seek, SeekFrom, Write};
+use std::io::{self, Read, Write};
 use std::os::raw::c_int;
-
-use libc::{fflush, fread, fseek, fwrite, FILE};
-
-pub struct File(*mut FILE);
-
-unsafe impl Send for File {}
-
-impl File {
-    pub fn new(f: *mut FILE) -> Self {
-        File(f)
-    }
-
-    pub unsafe fn raw(&mut self) -> *mut FILE {
-        self.0
-    }
-}
-
-impl Drop for File {
-    fn drop(&mut self) {
-        self.flush().unwrap()
-    }
-}
-
-impl Read for File {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        Ok(unsafe { fread(buf.as_mut_ptr() as *mut c_void, 1, buf.len(), self.0) })
-    }
-}
-
-impl Seek for File {
-    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
-        if let SeekFrom::Start(pos) = pos {
-            if unsafe { fseek(self.0, pos.try_into().unwrap(), libc::SEEK_SET) } < 0 {
-                Err(io::Error::new(io::ErrorKind::Other, "seek error"))
-            } else {
-                Ok(pos)
-            }
-        } else {
-            unimplemented!()
-        }
-    }
-}
-
-impl Write for File {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        Ok(unsafe { fwrite(buf.as_ptr() as *const c_void, 1, buf.len(), self.0) })
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        unsafe {
-            fflush(self.0);
-        }
-        Ok(())
-    }
-}
 
 pub struct FdFile(c_int);
 
