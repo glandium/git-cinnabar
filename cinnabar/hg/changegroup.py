4795a505b2ccbf4eae8093913f0c95b7331ef6af
Mike Hommey <mh@glandium.org>
2022-07-09 21:28:43 +0900

    Remove now unused cinnabar.hg.changegroup.RevDiff

--- cinnabar/hg/changegroup.py
+++ cinnabar/hg/changegroup.py
@@ -2,7 +2,6 @@ from binascii import (
     hexlify,
     unhexlify,
 )
-import struct
 from cinnabar.git import NULL_NODE_ID
 
 
@@ -31,42 +30,6 @@ class ParentsTrait(object):
             self.parent2 = NULL_NODE_ID
         if not parents:
             self.parent1 = NULL_NODE_ID
-
-
-class RevDiff(object):
-    class Part(object):
-        __slots__ = ('start', 'end', 'block_len', 'text_data')
-
-        def __init__(self, rev_patch):
-            self.start, self.end, self.block_len = \
-                struct.unpack('>lll', rev_patch[:12].tobytes())
-            self.text_data = rev_patch[12:12 + self.block_len]
-
-        def __len__(self):
-            return self.block_len + 12
-
-    def __init__(self, buf):
-        self._buf = memoryview(buf)
-
-    def __iter__(self):
-        start = 0
-        while start < len(self._buf):
-            part = self.Part(self._buf[start:])
-            yield part
-            start += len(part)
-
-    def apply(self, raw_orig):
-        new = bytearray()
-        orig = memoryview(raw_orig)
-        end = 0
-        for diff in self:
-            new += orig[end:diff.start]
-            new += diff.text_data
-            end = diff.end
-        if new == b'' and end == 0:
-            return raw_orig
-        new += orig[end:]
-        return bytes(new)
 
 
 class RawRevChunk(bytearray, ParentsTrait):
@@ -102,7 +65,7 @@ class RawRevChunk01(RawRevChunk):
     parent2 = RawRevChunk._field(40, 20, hexlify)
     changeset = RawRevChunk._field(60, 20, hexlify)
     data = RawRevChunk._field(80)
-    patch = RawRevChunk._field(80, filter=RevDiff)
+    patch = RawRevChunk._field(80)
 
     # Because we keep so many instances of this class on hold, the overhead
     # of having a __dict__ per instance is a deal breaker.
@@ -126,4 +89,4 @@ class RawRevChunk02(RawRevChunk):
     delta_node = RawRevChunk._field(60, 20, hexlify)
     changeset = RawRevChunk._field(80, 20, hexlify)
     data = RawRevChunk._field(100)
-    patch = RawRevChunk._field(100, filter=RevDiff)
+    patch = RawRevChunk._field(100)
