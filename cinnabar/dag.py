af78d5198a988ef54df53e37ee80f2571e51e74a
Mike Hommey <mh@glandium.org>
2022-07-21 09:46:16 +0900

    Remove now unused parts of cinnabar.dag.gitdag

--- cinnabar/dag.py
+++ cinnabar/dag.py
@@ -25,13 +25,6 @@ class gitdag(object):
         if tag:
             self._tags[node] = tag
 
-    def roots(self, tag=None):
-        for node, parents in self._parents.items():
-            if self._tags.get(node) == tag:
-                if all(p not in self._parents or self._tags.get(p) != tag
-                       for p in parents):
-                    yield node
-
     def heads(self, tag=None):
         for node in self._parents:
             if self._tags.get(node) == tag:
@@ -41,12 +34,6 @@ class gitdag(object):
                     yield node
 
     def tag_nodes_and_parents(self, nodes, tag):
-        self._tag_nodes_and_other(self._parents, nodes, tag)
-
-    def tag_nodes_and_children(self, nodes, tag):
-        self._tag_nodes_and_other(self._children, nodes, tag)
-
-    def _tag_nodes_and_other(self, other, nodes, tag):
         assert tag
         queue = deque(nodes)
         while queue:
@@ -54,24 +41,8 @@ class gitdag(object):
             if node in self._tags:
                 continue
             self._tags[node] = tag
-            for o in other.get(node, ()):
+            for o in self._parents.get(node, ()):
                 queue.append(o)
-
-    def iternodes(self, tag=None):
-        if tag is None:
-            for n in self._parents:
-                if n not in self._tags:
-                    yield n
-        else:
-            for n, t in self._tags.items():
-                if t == tag:
-                    yield n
 
     def __len__(self):
         return len(self._parents)
-
-    def __contains__(self, node):
-        return node in self._parents
-
-    def tags(self):
-        return set(self._tags.values())
--- tests/dag.py
+++ tests/dag.py
@@ -17,69 +17,28 @@ class TestDag(unittest.TestCase):
         ])
 
     def test_dag(self):
-        self.assertEqual(set(self.dag.roots()), set('BC'))
         self.assertEqual(set(self.dag.heads()), set('EGHIJ'))
 
     def test_tag_nodes_and_parents(self):
         self.dag.tag_nodes_and_parents('CD', 'foo')
-        self.assertEqual(set(self.dag.roots('foo')), set('BC'))
         self.assertEqual(set(self.dag.heads('foo')), set('CD'))
-        self.assertEqual(set(self.dag.roots()), set('EFGH'))
         self.assertEqual(set(self.dag.heads()), set('EGHIJ'))
 
         # Using a different tag for already tagged nodes doesn't change
         # anything
         self.dag.tag_nodes_and_parents('CD', 'bar')
-        self.assertEqual(set(self.dag.roots('bar')), set())
         self.assertEqual(set(self.dag.heads('bar')), set())
-        self.assertEqual(set(self.dag.roots()), set('EFGH'))
         self.assertEqual(set(self.dag.heads()), set('EGHIJ'))
 
     def test_tag_nodes_and_parents_2(self):
         self.dag.tag_nodes_and_parents('F', 'foo')
-        self.assertEqual(set(self.dag.roots('foo')), set('BC'))
         self.assertEqual(set(self.dag.heads('foo')), set('F'))
-        self.assertEqual(set(self.dag.roots()), set('DEIJ'))
         self.assertEqual(set(self.dag.heads()), set('EGHIJ'))
 
         self.dag.tag_nodes_and_parents('GHIJ', 'bar')
-        self.assertEqual(set(self.dag.roots('foo')), set('BC'))
         self.assertEqual(set(self.dag.heads('foo')), set('F'))
-        self.assertEqual(set(self.dag.roots('bar')), set('DIJ'))
         self.assertEqual(set(self.dag.heads('bar')), set('GHIJ'))
-        self.assertEqual(set(self.dag.roots()), set('E'))
         self.assertEqual(set(self.dag.heads()), set('E'))
 
         self.dag.tag_nodes_and_parents('E', 'baz')
-        self.assertEqual(set(self.dag.roots()), set())
-        self.assertEqual(set(self.dag.heads()), set())
-
-    def test_tag_nodes_and_children(self):
-        self.dag.tag_nodes_and_children('CD', 'foo')
-        self.assertEqual(set(self.dag.roots('foo')), set('CD'))
-        self.assertEqual(set(self.dag.heads('foo')), set('GHIJ'))
-        self.assertEqual(set(self.dag.roots()), set('B'))
-        self.assertEqual(set(self.dag.heads()), set('E'))
-
-        # Using a different tag for already tagged nodes doesn't change
-        # anything
-        self.dag.tag_nodes_and_children('CD', 'bar')
-        self.assertEqual(set(self.dag.roots('bar')), set())
-        self.assertEqual(set(self.dag.heads('bar')), set())
-        self.assertEqual(set(self.dag.roots()), set('B'))
-        self.assertEqual(set(self.dag.heads()), set('E'))
-
-    def test_tag_nodes_and_children_2(self):
-        self.dag.tag_nodes_and_children('C', 'foo')
-        self.assertEqual(set(self.dag.roots('foo')), set('C'))
-        self.assertEqual(set(self.dag.heads('foo')), set('IJ'))
-        self.assertEqual(set(self.dag.roots()), set('B'))
-        self.assertEqual(set(self.dag.heads()), set('EGH'))
-
-        self.dag.tag_nodes_and_children('B', 'bar')
-        self.assertEqual(set(self.dag.roots('foo')), set('C'))
-        self.assertEqual(set(self.dag.heads('foo')), set('IJ'))
-        self.assertEqual(set(self.dag.roots('bar')), set('B'))
-        self.assertEqual(set(self.dag.heads('bar')), set('EGH'))
-        self.assertEqual(set(self.dag.roots()), set())
         self.assertEqual(set(self.dag.heads()), set())
