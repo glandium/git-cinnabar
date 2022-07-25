aee9692d13316edcc55c02bc1fff99238c168083
Mike Hommey <mh@glandium.org>
2022-07-25 17:07:48 +0900

    Simplify remote_helper.py
    We don't need a separate class anymore, and stdout doesn't need to be
    binary anymore, as it only is used for list of commits/changesets.

--- cinnabar/remote_helper.py
+++ cinnabar/remote_helper.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 from cinnabar.hg.bundle import (
@@ -7,34 +6,16 @@ from cinnabar.hg.bundle import (
 )
 
 
-class GitRemoteHelper(object):
-    def __init__(self, store, stdin=sys.stdin.buffer,
-                 stdout=sys.stdout.buffer):
-        self._store = store
-        self._helper_in = stdin
-        self._helper = stdout
-
-    def bundle(self):
-        bundle_commits = []
-        while True:
-            line = self._helper_in.readline().strip()
-            if not line:
-                break
-            commit, _, parents = line.partition(b' ')
-            bundle_commits.append(
-                (commit, parents.split(b' ') if parents else []))
-
-        create_bundle(self._store, bundle_commits)
-
-
 def main(args):
-    if sys.platform == 'win32':
-        # By default, sys.stdout on Windows will transform \n into \r\n, which
-        # the calling git process won't recognize in our answers.
-        import msvcrt
-        msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
     store = PushStore()
 
-    helper = GitRemoteHelper(store)
+    bundle_commits = []
+    while True:
+        line = sys.stdin.buffer.readline().strip()
+        if not line:
+            break
+        commit, _, parents = line.partition(b' ')
+        bundle_commits.append(
+            (commit, parents.split(b' ') if parents else []))
 
-    getattr(helper, args[0])(*args[1:])
+    create_bundle(store, bundle_commits)
