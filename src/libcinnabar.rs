glandium/git-cinnabar

commit f20b5b334ff293df8522fd68107ab8eb6c4ae780

Remove generate_manifest
This removes the unsafeness of generate_manifest by making the buffer
store_manifest uses allocated by the caller. This has the disadvantage
of making the overall process of importing manifests even slower than
the move of generate_manifest to Rust made it.

src/libcinnabar.rs
@@ -37,6 +37,25 @@ impl<'a, T: AsRef<[u8]> + 'a> From<T> for strslice<'a> {
 
 #[allow(non_camel_case_types)]
 #[repr(C)]
+pub struct strslice_mut<'a> {
+    len: usize,
+    buf: *mut c_char,
+    marker: PhantomData<&'a mut [u8]>,
+}
+
+impl<'a, T: AsMut<[u8]> + 'a> From<T> for strslice_mut<'a> {
+    fn from(mut buf: T) -> Self {
+        let buf = buf.as_mut();
+        strslice_mut {
+            len: buf.len(),
+            buf: buf.as_mut_ptr() as *mut c_char,
+            marker: PhantomData,
+        }
+    }
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
 #[derive(Clone, Default)]
 pub struct hg_object_id([u8; 20]);
 

src/store.rs
@@ -41,7 +41,7 @@ use crate::hg_bundle::{
 };
 use crate::hg_connect_http::HttpRequest;
 use crate::hg_data::{hash_data, GitAuthorship, HgAuthorship, HgCommitter};
-use crate::libcinnabar::{files_meta, git2hg, hg2git, hg_object_id, strslice};
+use crate::libcinnabar::{files_meta, git2hg, hg2git, hg_object_id, strslice, strslice_mut};
 use crate::libgit::{
     changesets_oid, die, get_oid_blob, object_id, strbuf, Commit, RawBlob, RawCommit, RawTree,
     RefTransaction,
@@ -1285,24 +1285,8 @@ pub fn create_changeset(
 }
 
 extern "C" {
-    pub fn store_manifest(chunk: *const rev_chunk);
+    pub fn store_manifest(chunk: *const rev_chunk, reference_mn: strslice, stored_mn: strslice_mut);
     fn store_file(chunk: *const rev_chunk);
-}
-
-/// XXX: This is unsound. The string behind the returned slice may theoretically
-/// be freed and the rust compiler won't warn about it. But in practice, that's ok.
-/// The code using the slice is releasing it before returning, and we're
-/// single-threaded.
-#[no_mangle]
-pub unsafe extern "C" fn generate_manifest(oid: *const object_id) -> strslice<'static> {
-    mem::transmute::<strslice, _>(
-        RawHgManifest::read(
-            GitManifestId::from_raw_bytes(oid.as_ref().unwrap().as_raw_bytes()).unwrap(),
-        )
-        .unwrap()
-        .as_ref()
-        .into(),
-    )
 }
 
 #[no_mangle]
@@ -1418,9 +1402,45 @@ pub fn store_changegroup<R: Read>(input: R, version: u8) {
     for manifest in RevChunkIter::new(version, &mut input)
         .progress(|n| format!("Reading and importing {n} manifests"))
     {
-        unsafe {
-            store_manifest(&manifest);
+        let mid = HgManifestId::from_unchecked(HgObjectId::from(manifest.node().clone()));
+        let delta_node =
+            HgManifestId::from_unchecked(HgObjectId::from(manifest.delta_node().clone()));
+        let reference_mn = if delta_node.is_null() {
+            RawHgManifest::empty()
+        } else {
+            RawHgManifest::read(delta_node.to_git().unwrap()).unwrap()
+        };
+        let mut last_end = 0;
+        let mut mn_size = 0;
+        for diff in manifest.iter_diff() {
+            if diff.start > reference_mn.len() || diff.start < last_end {
+                die!("Malformed changeset chunk for {mid}");
+            }
+            mn_size += diff.start - last_end;
+            mn_size += diff.data.len();
+            last_end = diff.end;
         }
+        if reference_mn.len() < last_end {
+            die!("Malformed changeset chunk for {mid}");
+        }
+        mn_size += reference_mn.len() - last_end;
+
+        let mut stored_manifest = vec![0; mn_size];
+        unsafe {
+            store_manifest(
+                &manifest,
+                (&reference_mn).into(),
+                (&mut stored_manifest).into(),
+            );
+        }
+        let stored_manifest = Rc::<[u8]>::from(stored_manifest.as_ref());
+        let tree_id = mid.to_git().unwrap().get_tree_id();
+        MANIFESTCACHE.with(|cache| {
+            cache.set(Some(ManifestCache {
+                tree_id,
+                content: stored_manifest,
+            }));
+        });
     }
     let files = Cell::new(0);
     let mut progress = repeat(()).progress(|n| {

src/cinnabar-fast-import.c
@@ -634,13 +634,15 @@ static void manifest_metadata_path(struct strbuf *out, struct strslice *in)
 	strbuf_addslice(out, *in);
 }
 
-void store_manifest(struct rev_chunk *chunk)
+void store_manifest(struct rev_chunk *chunk,
+                    const struct strslice last_manifest_content,
+                    struct strslice_mut stored_manifest)
 {
 	static struct hg_object_id last_manifest_oid;
 	static struct branch *last_manifest;
-	static struct strbuf last_manifest_content = STRBUF_INIT;
-	struct strbuf data = STRBUF_INIT;
 	struct strbuf path = STRBUF_INIT;
+	struct strbuf data = STRBUF_INIT;
+	struct strslice_mut manifest = stored_manifest;
 	struct rev_diff_part diff;
 	size_t last_end = 0;
 	struct strslice slice;
@@ -659,7 +661,7 @@ void store_manifest(struct rev_chunk *chunk)
 		oidclr(&last_manifest->branch_tree.versions[1].oid);
 		hg_oidclr(&last_manifest_oid);
 		oidclr(&last_manifest->oid);
-		strbuf_reset(&last_manifest_content);
+		assert(last_manifest_content.len == 0);
 	} else if (!hg_oideq(chunk->delta_node, &last_manifest_oid)) {
 		const struct object_id *note;
 		ensure_notes(&hg2git);
@@ -681,26 +683,22 @@ void store_manifest(struct rev_chunk *chunk)
 		oidcpy(&last_manifest->oid, note);
 		parse_from_existing(last_manifest);
 		load_tree(&last_manifest->branch_tree);
-		strbuf_reset(&last_manifest_content);
-		strbuf_addslice(&last_manifest_content, generate_manifest(note));
 	}
 
-	// Start with the same allocation size as last manifest. (-1 before
-	// strbuf_grow always adds 1 for a final '\0')
-	if (last_manifest_content.alloc)
-		strbuf_grow(&data, last_manifest_content.alloc - 1);
-	// While not exact, the total length of the previous manifest and the
-	// chunk will be an upper bound on the size of the new manifest, so
-	// ensure we'll have enough room for that.
-	strbuf_grow(&data, last_manifest_content.len + chunk->raw.len);
 	rev_diff_start_iter(&diff, chunk);
 	while (rev_diff_iter_next(&diff)) {
+		size_t len;
 		if (diff.start > last_manifest_content.len ||
 		    diff.start < last_end || diff.start > diff.end)
 			goto malformed;
-		strbuf_add(&data, last_manifest_content.buf + last_end,
-		           diff.start - last_end);
-		strbuf_addslice(&data, diff.data);
+		len = diff.start - last_end;
+		strslice_copy(
+			strslice_slice(last_manifest_content, last_end, len),
+			strslice_mut_slice(manifest, 0, diff.start - last_end));
+		manifest = strslice_mut_slice(manifest, len, SIZE_MAX);
+		strslice_copy(diff.data,
+		              strslice_mut_slice(manifest, 0, diff.data.len));
+		manifest = strslice_mut_slice(manifest, diff.data.len, SIZE_MAX);
 
 		last_end = diff.end;
 
@@ -715,8 +713,8 @@ void store_manifest(struct rev_chunk *chunk)
 		// TODO: Avoid a remove+add cycle for same-file modifications.
 
 		// Process removed files.
-		slice = strbuf_slice(&last_manifest_content, diff.start,
-                                     diff.end - diff.start);
+		slice = strslice_slice(last_manifest_content, diff.start,
+                                       diff.end - diff.start);
 		while (split_manifest_line(&slice, &line) == 0) {
 			manifest_metadata_path(&path, &line.path);
 			tree_content_remove(&last_manifest->branch_tree,
@@ -767,11 +765,9 @@ void store_manifest(struct rev_chunk *chunk)
 	if (last_manifest_content.len < last_end)
 		goto malformed;
 
-	strbuf_add(&data, last_manifest_content.buf + last_end,
-		   last_manifest_content.len - last_end);
-
-	strbuf_swap(&last_manifest_content, &data);
-	strbuf_release(&data);
+	strslice_copy(
+		strslice_slice(last_manifest_content, last_end, SIZE_MAX),
+		manifest);
 
 	store_tree(&last_manifest->branch_tree);
 	oidcpy(&last_manifest->branch_tree.versions[0].oid,

src/cinnabar-fast-import.h
@@ -5,6 +5,8 @@
 #ifndef CINNABAR_FAST_IMPORT_H
 #define CINNABAR_FAST_IMPORT_H
 
+#include "strslice.h"
+
 struct reader;
 struct object_id;
 struct hg_object_id;
@@ -37,7 +39,9 @@ void do_set(const char *what, const struct hg_object_id *hg_id,
             const struct object_id *git_id);
 
 void store_file(struct rev_chunk *chunk);
-void store_manifest(struct rev_chunk *chunk);
+void store_manifest(struct rev_chunk *chunk,
+                    const struct strslice last_manifest_content,
+                    struct strslice_mut data);
 void store_metadata_notes(
 	struct cinnabar_notes_tree *notes, const struct object_id *reference,
 	struct object_id *result);

src/cinnabar-helper.h
@@ -31,8 +31,6 @@ extern struct notes_tree git2hg, hg2git, files_meta;
 
 extern void ensure_notes(struct notes_tree *notes);
 
-struct strslice generate_manifest(const struct object_id *oid);
-
 int check_manifest(const struct object_id *oid);
 
 struct remote;

src/main.rs
@@ -137,7 +137,7 @@ use crate::hg_bundle::BundleReader;
 use crate::hg_connect::{decodecaps, find_common, UnbundleResponse};
 use crate::progress::set_progress;
 use crate::store::{do_set_replace, reset_manifest_heads, set_changeset_heads, Dag, Traversal};
-use crate::tree_util::{ParseTree, WithPath};
+use crate::tree_util::{Empty, ParseTree, WithPath};
 use crate::util::{FromBytes, ToBoxed};
 
 #[cfg(any(feature = "version-check", feature = "self-update"))]
@@ -1526,10 +1526,13 @@ fn create_copy(blobid: BlobId, source_path: &BStr, source_fid: HgFileId) -> HgFi
     fid
 }
 
-fn create_manifest(content: &[u8], parents: &[HgManifestId]) -> HgManifestId {
-    let parent_manifest = parents
-        .get(0)
-        .map(|p| RawHgManifest::read(p.to_git().unwrap()).unwrap());
+// content is &mut but is only going to be overwritten with the same content.
+// This is an inconvenience from the way store_manifest currently works, and
+// it will remain this way until it moves to Rust.
+fn create_manifest(content: &mut [u8], parents: &[HgManifestId]) -> HgManifestId {
+    let parent_manifest = parents.get(0).map_or_else(RawHgManifest::empty, |p| {
+        RawHgManifest::read(p.to_git().unwrap()).unwrap()
+    });
     let parent1 = parents.get(0).copied().unwrap_or(HgManifestId::NULL);
     let mut hash = HgManifestId::create();
     if parents.len() < 2 {
@@ -1541,15 +1544,9 @@ fn create_manifest(content: &[u8], parents: &[HgManifestId]) -> HgManifestId {
             hash.update(parent.as_raw_bytes());
         }
     }
-    hash.update(content);
+    hash.update(&content);
     let mid = hash.finalize();
-    let data = create_chunk_data(
-        parent_manifest
-            .as_ref()
-            .map(|m| m.as_bstr())
-            .unwrap_or_default(),
-        content,
-    );
+    let data = create_chunk_data(&parent_manifest, content);
     let mut manifest_chunk = Vec::new();
     manifest_chunk.extend_from_slice(b"\0\0\0\0");
     manifest_chunk.extend_from_slice(mid.as_raw_bytes());
@@ -1565,7 +1562,7 @@ fn create_manifest(content: &[u8], parents: &[HgManifestId]) -> HgManifestId {
     manifest_chunk.extend_from_slice(b"\0\0\0\0");
     for chunk in RevChunkIter::new(2, manifest_chunk.as_bytes()) {
         unsafe {
-            store_manifest(&chunk);
+            store_manifest(&chunk, (&parent_manifest).into(), content.into());
         }
     }
     mid
@@ -1593,7 +1590,7 @@ fn create_root_changeset(cid: CommitId) -> HgChangesetId {
         paths.push(b'\0');
     }
     paths.pop();
-    let mid = create_manifest(&manifest, &[]);
+    let mid = create_manifest(&mut manifest, &[]);
     let (csid, _) = create_changeset(cid, mid, Some(paths.to_boxed()));
     csid
 }
@@ -1709,7 +1706,7 @@ fn create_simple_manifest(cid: CommitId, parent: CommitId) -> (HgManifestId, Opt
         paths.push(b'\0');
     }
     paths.pop();
-    let mid = create_manifest(&manifest, &[parent_mid]);
+    let mid = create_manifest(&mut manifest, &[parent_mid]);
     (mid, Some(paths.into_boxed_slice()))
 }
 
@@ -1892,7 +1889,7 @@ fn create_merge_changeset(
             (parent1_mid, None)
         } else {
             (
-                create_manifest(&manifest, &[parent1_mid, parent2_mid]),
+                create_manifest(&mut manifest, &[parent1_mid, parent2_mid]),
                 Some(paths.into_boxed_slice()),
             )
         };

src/strslice.h
@@ -5,12 +5,18 @@
 #ifndef STRSLICE_H
 #define STRSLICE_H
 
+#include <assert.h>
 #include <string.h>
 #include "strbuf.h"
 
 struct strslice {
 	size_t len;
 	const char *buf;
+};
+
+struct strslice_mut {
+	size_t len;
+	char *buf;
 };
 
 static inline
@@ -31,6 +37,20 @@ static inline
 struct strslice strslice_slice(struct strslice slice, size_t start, size_t len)
 {
 	struct strslice result;
+	if (start >= slice.len)
+		len = 0;
+	else if (slice.len - start < len)
+		len = slice.len - start;
+	result.len = len;
+	result.buf = len ? slice.buf + start : strbuf_slopbuf;
+	return result;
+}
+
+static inline
+struct strslice_mut strslice_mut_slice(struct strslice_mut slice, size_t start,
+                                       size_t len)
+{
+	struct strslice_mut result;
 	if (start >= slice.len)
 		len = 0;
 	else if (slice.len - start < len)
@@ -114,6 +134,13 @@ static inline int strslice_startswith(const struct strslice a,
 	} else {
 		return 0;
 	}
+}
+
+static inline void strslice_copy(const struct strslice from,
+                                 struct strslice_mut to)
+{
+	assert(from.len == to.len);
+	memcpy(to.buf, from.buf, to.len);
 }
 
 #endif

commit e458f882745d66251358c8890bb9d7b23c77f7fc

Allow to create Rc<[u8]> from a pseudo-Vec without a copy
This compensates for the performance regression from f20b5b334.

src/libcinnabar.rs
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use std::marker::PhantomData;
+use std::mem::MaybeUninit;
 use std::os::raw::{c_char, c_int, c_void};
 
 use crate::git::GitObjectId;
@@ -43,9 +44,18 @@ pub struct strslice_mut<'a> {
     marker: PhantomData<&'a mut [u8]>,
 }
 
-impl<'a, T: AsMut<[u8]> + 'a> From<T> for strslice_mut<'a> {
-    fn from(mut buf: T) -> Self {
-        let buf = buf.as_mut();
+impl<'a> From<&'a mut [u8]> for strslice_mut<'a> {
+    fn from(buf: &'a mut [u8]) -> Self {
+        strslice_mut {
+            len: buf.len(),
+            buf: buf.as_mut_ptr() as *mut c_char,
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<'a> From<&'a mut [MaybeUninit<u8>]> for strslice_mut<'a> {
+    fn from(buf: &'a mut [MaybeUninit<u8>]) -> Self {
         strslice_mut {
             len: buf.len(),
             buf: buf.as_mut_ptr() as *mut c_char,

src/store.rs
@@ -49,7 +49,9 @@ use crate::libgit::{
 use crate::oid::ObjectId;
 use crate::progress::{progress_enabled, Progress};
 use crate::tree_util::{diff_by_path, Empty, ParseTree, RecurseTree};
-use crate::util::{FromBytes, ImmutBString, OsStrExt, ReadExt, SliceExt, ToBoxed, Transpose};
+use crate::util::{
+    FromBytes, ImmutBString, OsStrExt, RcExt, ReadExt, SliceExt, ToBoxed, Transpose,
+};
 use crate::xdiff::{apply, textdiff, PatchInfo};
 use crate::{check_enabled, do_reload, Checks};
 
@@ -574,7 +576,7 @@ impl RawHgManifest {
             let last_manifest = cache.take();
             let tree_id = oid.get_tree_id();
 
-            let mut manifest = Vec::new();
+            let mut manifest = Rc::<[u8]>::builder();
             if let Some(last_manifest) = last_manifest {
                 let reference_manifest = last_manifest.content.clone();
                 if last_manifest.tree_id == tree_id {
@@ -639,7 +641,7 @@ impl RawHgManifest {
                     RawHgManifest::write_one_entry(&entry, &mut manifest).unwrap();
                 }
             }
-            let content = Rc::<[u8]>::from(manifest.as_ref());
+            let content = manifest.into_rc();
 
             cache.set(Some(ManifestCache {
                 tree_id,
@@ -1426,20 +1428,21 @@ pub fn store_changegroup<R: Read>(input: R, version: u8) {
         }
         mn_size += reference_mn.len() - last_end;
 
-        let mut stored_manifest = vec![0; mn_size];
+        let mut stored_manifest = Rc::builder_with_capacity(mn_size);
         unsafe {
             store_manifest(
                 &manifest,
                 (&reference_mn).into(),
-                (&mut stored_manifest).into(),
+                (&mut stored_manifest.spare_capacity_mut()[..mn_size]).into(),
             );
+            stored_manifest.set_len(mn_size);
         }
-        let stored_manifest = Rc::<[u8]>::from(stored_manifest.as_ref());
+
         let tree_id = mid.to_git().unwrap().get_tree_id();
         MANIFESTCACHE.with(|cache| {
             cache.set(Some(ManifestCache {
                 tree_id,
-                content: stored_manifest,
+                content: stored_manifest.into_rc(),
             }));
         });
     }

src/util.rs
@@ -2,12 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use std::alloc::Layout;
+use std::cell::Cell;
+use std::cmp;
 use std::ffi::{CStr, CString, OsStr};
 use std::io::{self, LineWriter, Read, Write};
+use std::marker::PhantomData;
+use std::mem::MaybeUninit;
+use std::ops::{Deref, DerefMut};
 #[cfg(unix)]
 use std::os::unix::ffi;
 #[cfg(windows)]
 use std::os::windows::ffi;
+use std::ptr::{self, NonNull};
+use std::rc::Rc;
 use std::str::{self, FromStr};
 use std::{fmt, mem};
 
@@ -431,4 +439,191 @@ pub trait Transpose {
     type Target;
 
     fn transpose(self) -> Self::Target;
+}
+
+type RcBox = [Cell<usize>; 2];
+
+pub struct RcSliceBuilder<T> {
+    ptr: NonNull<T>,
+    len: usize,
+    capacity: usize,
+    marker: PhantomData<T>,
+}
+
+impl<T> RcSliceBuilder<T> {
+    pub fn new() -> Self {
+        RcSliceBuilder {
+            ptr: NonNull::dangling(),
+            len: 0,
+            capacity: 0,
+            marker: PhantomData,
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        let mut result = Self::new();
+        if capacity > 0 {
+            result.grow_to(capacity);
+        }
+        result
+    }
+
+    pub fn into_rc(self) -> Rc<[T]> {
+        if self.len != 0 {
+            let (layout, offset) = Self::layout_for_size(self.len);
+            let ptr = if layout.size() != self.capacity {
+                let (current_layout, _) = Self::layout_for_size(self.capacity);
+                // We need to shrink to fit so that Rc's deallocation layout matches ours.
+                unsafe {
+                    let ptr = std::alloc::realloc(
+                        self.ptr.cast::<u8>().as_ptr().sub(offset),
+                        current_layout,
+                        layout.size(),
+                    );
+                    if ptr.is_null() {
+                        panic!("Out of memory");
+                    }
+                    NonNull::new_unchecked(ptr.add(offset) as *mut T)
+                }
+            } else {
+                self.ptr
+            };
+            let len = self.len;
+            mem::forget(self);
+            unsafe {
+                ptr::write(
+                    ptr.cast::<u8>().as_ptr().sub(offset) as *mut RcBox,
+                    [Cell::new(1), Cell::new(1)],
+                );
+                Rc::from_raw(NonNull::slice_from_raw_parts(ptr, len).as_ptr())
+            }
+        } else {
+            Rc::new([])
+        }
+    }
+
+    fn layout_for_size(size: usize) -> (Layout, usize) {
+        Layout::array::<T>(size)
+            .and_then(|layout| Layout::new::<RcBox>().extend(layout))
+            .map(|(layout, offset)| (layout.pad_to_align(), offset))
+            .unwrap()
+    }
+
+    #[inline(never)]
+    fn grow_to(&mut self, needed_len: usize) {
+        // Same growth strategy as git's ALLOC_GROW.
+        let new_capacity = cmp::max(
+            needed_len,
+            self.capacity
+                .checked_add(16)
+                .and_then(|cap| cap.checked_mul(3))
+                .map_or(needed_len, |cap| cap / 2),
+        );
+        let (layout, offset) = Self::layout_for_size(new_capacity);
+        unsafe {
+            let ptr = if self.capacity == 0 {
+                std::alloc::alloc(layout)
+            } else {
+                let (current_layout, _) = Self::layout_for_size(self.capacity);
+                std::alloc::realloc(
+                    self.ptr.cast::<u8>().as_ptr().sub(offset),
+                    current_layout,
+                    layout.size(),
+                )
+            };
+            if ptr.is_null() {
+                panic!("Out of memory");
+            }
+            self.ptr = NonNull::new_unchecked(ptr.add(offset) as *mut T);
+            self.capacity = layout.size() - offset;
+        }
+    }
+
+    #[inline(always)]
+    pub fn reserve(&mut self, additional: usize) {
+        let new_len = self.len.checked_add(additional).unwrap();
+        if new_len > self.capacity {
+            self.grow_to(new_len);
+        }
+    }
+}
+
+impl<T: Copy> RcSliceBuilder<T> {
+    pub fn extend_from_slice(&mut self, other: &[T]) {
+        self.reserve(other.len());
+        unsafe {
+            ptr::copy_nonoverlapping(other.as_ptr(), self.ptr.as_ptr().add(self.len), other.len());
+        }
+        self.len += other.len();
+    }
+
+    pub fn spare_capacity_mut(&mut self) -> &mut [MaybeUninit<T>] {
+        unsafe {
+            std::slice::from_raw_parts_mut(
+                self.ptr.as_ptr().add(self.len) as *mut MaybeUninit<T>,
+                self.capacity - self.len,
+            )
+        }
+    }
+
+    pub unsafe fn set_len(&mut self, new_len: usize) {
+        debug_assert!(new_len <= self.capacity);
+        self.len = new_len;
+    }
+}
+
+impl<T> Deref for RcSliceBuilder<T> {
+    type Target = [T];
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.len) }
+    }
+}
+
+impl<T> DerefMut for RcSliceBuilder<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { std::slice::from_raw_parts_mut(self.ptr.as_mut(), self.len) }
+    }
+}
+
+impl<T> Drop for RcSliceBuilder<T> {
+    fn drop(&mut self) {
+        unsafe {
+            ptr::drop_in_place(NonNull::slice_from_raw_parts(self.ptr, self.len).as_ptr());
+            if self.capacity > 0 {
+                let (layout, offset) = Self::layout_for_size(self.capacity);
+                std::alloc::dealloc(self.ptr.cast::<u8>().as_ptr().sub(offset), layout);
+            }
+        }
+    }
+}
+
+impl Write for RcSliceBuilder<u8> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.extend_from_slice(buf);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+pub trait RcExt {
+    type Builder;
+
+    fn builder() -> Self::Builder;
+    fn builder_with_capacity(capacity: usize) -> Self::Builder;
+}
+
+impl<T> RcExt for Rc<[T]> {
+    type Builder = RcSliceBuilder<T>;
+
+    fn builder() -> Self::Builder {
+        Self::Builder::new()
+    }
+
+    fn builder_with_capacity(capacity: usize) -> Self::Builder {
+        Self::Builder::with_capacity(capacity)
+    }
 }