1d54b831c7c11292ae8d3eb7379cdf30160aaeed
Mike Hommey <mh@glandium.org>
2024-06-20 05:40:36 +0900

    Revert "Use DurationExt::fuzzy_display in Progress too"
    This reverts commit 49e6a8fad85ae38560d3f1ac3c412671599b5147.
    
    This wasn't a great idea, because the assumption in the progress-meter
    is that the printed string never shrinks, but fuzzy_display output can.

--- src/progress.rs
+++ src/progress.rs
@@ -6,7 +6,6 @@ use std::iter::Enumerate;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Instant;
 
-use crate::util::DurationExt;
 use crate::{check_enabled, Checks};
 
 static PROGRESS_ENABLED: AtomicBool = AtomicBool::new(true);
@@ -62,7 +61,7 @@ impl<I: Iterator, F: Fn(usize) -> String> ProgressIterEnabled<I, F> {
     fn display(&mut self, now: Instant) {
         let s = (self.formatter)(self.count);
         if let Some(start) = self.start {
-            eprint!("\r{} in {}", s, (now - start).fuzzy_display());
+            eprint!("\r{} in {:.1}s", s, (now - start).as_secs_f32());
         } else {
             eprint!("\r{}", s);
         }
