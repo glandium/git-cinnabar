caaff079e5c6f276fa227c8fd3d23f1dc907094f
Mike Hommey <mh@glandium.org>
2024-08-01 18:41:00 +0900

    [CI] Explicitly statically link liblzma and libbzip2

--- CI/tools.py
+++ CI/tools.py
@@ -394,7 +394,7 @@ class Build(Task, metaclass=Tool):
             "WARNINGS_AS_ERRORS": "1",
         }
         cargo_flags = ["-vv", "--release"]
-        cargo_features = ["self-update", "gitdev"]
+        cargo_features = ["self-update", "gitdev", "xz2/static", "bzip2/static"]
         rust_version = None
         if variant == "asan":
             if os.endswith("osx"):
