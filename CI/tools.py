19c61d6f60ada6206eb6a9c05b560335bd91291b
Mike Hommey <mh@glandium.org>
2024-12-11 09:54:55 +0900

    Update git-core to v2.47.1

--- CI/tools.py
+++ CI/tools.py
@@ -17,7 +17,7 @@ from util import build_commit
 from variables import TC_BRANCH, TC_IS_PUSH
 
 MERCURIAL_VERSION = "6.8"
-GIT_VERSION = "2.47.0"
+GIT_VERSION = "2.47.1"
 
 ALL_MERCURIAL_VERSIONS = (
     "1.9.3",
